8e7049c5532afd9d2012af44943720506c40b4ce
Bepo <48807766+Veronidas@users.noreply.github.com>
2019-09-13 10:54:22 -0300

    Add files via upload

---
+++ 7.js
@@ -0,0 +1,7 @@
+//Cadena a convertir
+let name = "veronika";
+//iteracion a la cadena
+for (i = 0; i < name.length; i++) {
+//imprimimos en la consola el nombre, haciendo que imprima por separado cada iteracion y le aplicamos el metodo toUpperCase para que este todo en mayusculas
+  console.log(name[i].toUpperCase());
+  }
