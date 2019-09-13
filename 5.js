40580fda225453e51e0af68216e3f2a7e4b09af0
Bepo <48807766+Veronidas@users.noreply.github.com>
2019-09-13 10:59:45 -0300

    Update 5.js

--- 5.js
+++ 5.js
@@ -1,11 +1,15 @@
+//array a trabajar
 let array=[3,24,12,58,10,67,40,43]
+//establecemos nuestro numero mayor comenzando con 0
 let biggest = 0;
- 
+ //hacemos la iteracion del array
 for(i = 0; i < array.length; i++){
+ //establecemos nuestra condicion, si el numero iterado es mayor que biggest, que es nuestro numero mayor entonces:
     if (array[i] > biggest)
     {
-        biggest = array[i];
+     //entonces ahora biggest seria ese numero superior al valor que estaba anteriormente, y al estar iterando recorre todo el arreglo hasta revisar cual es el mayor sustituyendolo en la variable biggest
+     biggest = array[i];
     }
 }
- 
+ //se imoprime
 console.log(biggest);
